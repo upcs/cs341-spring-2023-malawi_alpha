cd1463eb5e24ccbe055c7814d73d2fdd4076d21d
cachola25 <cachola25@up.edu>
2023-02-21 00:04:56 -0800

    Updated functionality

--- teacher-view.js
+++ teacher-view.js
@@ -36,12 +36,16 @@ $(function () {
   });
 
   const tokenButton = document.getElementById("token-button");
-  tokenButtonOnClick = function (event) {
+  const prinoutButton = document.getElementById("printout-button");
+  const gradesheet = document.getElementById("gradesheet-body");
+  const newStudentButton = document.getElementById("new-student-button");
+
+  tokenButtonHandler = function (event) {
     var token = $("#token-text").val();
     const accessToken = "token";
     console.log(token);
     console.log(accessToken);
-    if (token == accessToken) {
+    if (token === accessToken) {
       alert("You are now in edit mode. Click on a grade to edit it.");
       var tdList = document.querySelectorAll("td.grades");
       // loop through the td elements and set their contenteditable property to true
@@ -52,5 +56,28 @@ $(function () {
       alert("Invalid token. Please try again.");
     }
   };
-  tokenButton.addEventListener("click", tokenButtonOnClick);
+
+  printoutHandler = function (event) {
+    window.location.replace("/printout.html")
+  };
+
+  addNewStudent = function (event) {
+    console.log("new student button clicked");
+    let length = gradesheet.rows.length;
+    var newRow = document.createElement("tr"); // Create new row element
+    for (var i = 0; i < 20; i++) {
+      var newCell = document.createElement("td"); // Create new cell element
+      if(i == 1) {
+        newCell.innerText = "Student " + length;
+      } else {
+        newCell.className = "grades";
+      }
+      newRow.appendChild(newCell); // Append the new cell to the new row
+    }
+    gradesheet.appendChild(newRow);
+  };
+
+  tokenButton.addEventListener("click", tokenButtonHandler);
+  prinoutButton.addEventListener("click", printoutHandler);
+  newStudentButton.addEventListener("click", addNewStudent);
 });
