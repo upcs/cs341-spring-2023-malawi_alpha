627f9fec261950aad08175357475ecfc614518da
Megan <lym24@up.edu>
2023-03-16 12:41:06 -0700

    adminPage updates
    Made javascipt working

--- adminPage.js
+++ adminPage.js
@@ -5,19 +5,18 @@ $(function(){
     const saveButton = document.getElementById("save-button");
 
     editTokenHandler = function (event){
-        var adminToken = $("#token-entry").val();
+        var inputToken = $("#token-entry").val();
         const adminToken = "tempToken";
         console.log(adminToken);
         console.log(inputToken);
-        if(token == inputToken){
-            console.alert("Token is correct, you are now in editing mode. Don't forget to save before closing.");
+        if(inputToken = adminToken){
+            window.alert("Token is correct, you are now in editing mode. Don't forget to save before closing.");
             //be able to edit the cells, add classes into the classes collum, and edit the teacher collumn 
 
         }
         else{
             console.alert("Token is incorrect, please try again.");
         }
-
     };
 
     generateTokenHandler = function (event){
@@ -25,23 +24,22 @@ $(function(){
         console.log(newToken);
         var table = document.getElementById("tokensheet");
 
-
         //add new row to the table and new token into the first cell
         function newRow(){
             var row = table.insertRow(1);
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
-            cell1.innerHTML = print(newToken);
+            cell1.innerHTML = console.log(newToken);
             cell2.innerHTML = "New Cell2";
             cell3.innerHTML = "New Cell3";
         }
-    };
+        newRow();
+    }
 
-    //pushing to personal branch
     saveButtonHandler = function (event){
         console.log("Save button clicked");
-        console.alert("All changes have been saved, it is now safe to exit.");
+        window.alert("All changes have been saved, it is now safe to exit.");
     }
 
 
