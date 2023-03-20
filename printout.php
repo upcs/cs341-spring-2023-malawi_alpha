e77149c36669005ad467cac342956e92ac7387e9
Augustine Pham <pham24@up.edu>
2023-03-20 07:53:35 +0000

    Updating php functionality

---
+++ printout.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<title>Printout Page</title>
+<h1>Print Out</h1>
+</html>
---
+++ view-functionality.php
@@ -0,0 +1,26 @@
+<?php
+
+function newStudentButton() {
+
+	echo "new function working";
+}
+
+// $new_row = $dom->createElement('tr');
+
+// for ($i = 0; $i < 20; $i++) {
+
+//     $new_cell = $dom->createElement('td');
+//     if($i == 1) {
+//         $new_cell = $dom->createElement('td',"Student");
+//     } else {
+//         $new_cell->setAttribute('class','grades');
+//     }
+//     $new_row->appendChild($new_cell);
+// }
+
+// $gradesheet_body = $dom->getElementById("gradesheet-body");
+// $gradesheet_body->appendChild($new_row);
+// $gradesheet = $dom->getElementById("gradesheet");
+//echo $dom->saveHTMLFile("/var/www/html/teacher-view.php");
+// echo $dom->saveHTML();
+?>
