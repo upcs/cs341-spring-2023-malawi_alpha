57f066bf19e665c8a7f4659f9539db0e3a56c0bd
Augustine Pham <pham24@up.edu>
2023-03-21 15:13:04 -0700

    updated tests for formatting

--- testing/loginTeacherTest.py
+++ testing/loginTeacherTest.py
@@ -1,12 +1,17 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-driver.get("http://10.12.116.141")
-print(driver.title)
-login = driver.find_element(By.ID, "token")
-login.send_keys("12345")
-login.submit()
-print(driver.current_url)
-driver.close()
+class testLoginTeacher(unittest.TestCase):
+    def runTest(self):
+        driver = webdriver.Chrome(PATH)
+        driver.get("http://10.12.116.141")
+        print(driver.title)
+        login = driver.find_element(By.ID, "token")
+        login.send_keys("12345")
+        login.submit()
+        print(driver.current_url)
+        assert "teacher" in driver.current_url
+        driver.close()
+unittest.main()
--- testing/testingTest.py
+++ testing/testingTest.py
@@ -1,5 +1,10 @@
+import unittest
 from selenium import webdriver
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-driver.get("https://www.google.com")
-driver.quit()
+class testGoogle(unittest.TestCase):
+    def runTest(self):
+        driver = webdriver.Chrome(PATH)
+        driver.get("https://www.google.com")
+        assert "Google" in driver.title
+        driver.quit()
+unittest.main()
