cbdb2d3a60f69b78d3af84dd42ae865d0e1cc54e
Augustine Pham <pham24@up.edu>
2023-03-21 14:41:19 -0700

    added testing for Admin login

---
+++ testing/loginAdminTest.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("http://10.12.116.141")
+print(driver.title)
+login = driver.find_element(By.ID, "token")
+login.send_keys("54321")
+login.submit()
+print(driver.current_url)
+driver.close()
